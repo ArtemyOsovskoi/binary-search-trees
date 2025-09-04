160024e5c93cbf4d1ffd2649a7c5d576bf68c5f9
a_osovsky <116560582+ArtemyOsovskoi@users.noreply.github.com>
2025-09-04 17:43:27 +0300

    first commit node class

---
+++ script.js
@@ -0,0 +1,10 @@
+export default class Node {
+    constructor(data) {
+        this.leftChild = null; //left child reference
+        this.rightChild = null; //right child reference
+        this.data = data; //data stored in node
+    }
+
+
+
+}
