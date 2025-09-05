bcf2aa1560024223ebea2110695841f05f635e63
a_osovsky <116560582+ArtemyOsovskoi@users.noreply.github.com>
2025-09-05 17:36:14 +0300

    tree class

--- script.js
+++ script.js
@@ -5,6 +5,35 @@ export default class Node {
         this.data = data; //data stored in node
     }
 
+}
+
+export class Tree {
+    constructor(array) {
+        this.root = buildTree(array);
+    }
+}
+
+/* Write a buildTree(array) function that takes an array of data 
+(e.g., [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]) 
+and turns it into a balanced binary tree full of Node objects appropriately placed 
+(don’t forget to sort and remove duplicates!). 
+The buildTree function should return the level-0 root node. */
+function buildTree(array) {
+
 
 
 }
+
+//BST visualization function
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
